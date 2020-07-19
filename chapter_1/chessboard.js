07d9917964dd1484ae0e19da1f4fe5a216c4a8c5
GuRuGu <peter.k.nets@gmail.com>
2020-07-19 17:59:36 +0300

    Implement chessboard

---
+++ chapter_1/chessboard.js
@@ -0,0 +1,18 @@
+const printChessboard = (size = 8) => {
+  let chessboard = "";
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if ((i + j) % 2 === 0) {
+        chessboard += " ";
+      } else {
+        chessboard += "#";
+      }
+    }
+    chessboard += "\n";
+  }
+
+  console.log(chessboard);
+};
+
+printChessboard(12);
